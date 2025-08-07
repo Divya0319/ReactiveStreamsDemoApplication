d19923180010aa1f1362ceda136429ab1fb059ab
divya0319 <divygupta0319@gmail.com>
2025-08-07 23:21:30 +0530

    Added API documentation using springdoc-openapi

--- EmployeeWebClientConsumer/src/main/java/com/fastturtle/EmployeeWebClientConsumer/controllers/EmployeeClientController.java
+++ EmployeeWebClientConsumer/src/main/java/com/fastturtle/EmployeeWebClientConsumer/controllers/EmployeeClientController.java
@@ -2,12 +2,17 @@ package com.fastturtle.EmployeeWebClientConsumer.controllers;
 
 import com.fastturtle.EmployeeWebClientConsumer.dtos.Employee;
 import com.fastturtle.EmployeeWebClientConsumer.services.EmployeeClient;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/client/employees")
+@Tag(name = "EmployeeClient", description = "REST API for Employee CRUD operations.")
 public class EmployeeClientController {
 
     private final EmployeeClient employeeClient;
@@ -16,12 +21,42 @@ public class EmployeeClientController {
         this.employeeClient = employeeClient;
     }
 
-    @GetMapping
+    @Operation(
+            summary =
+                    "${api.employee-client.get-all-employees.description}",
+            description =
+                    "${api.employee-client.get-all-employees.notes}")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description =
+                    "${api.responseCodes.ok.description}"),
+            @ApiResponse(responseCode = "400", description =
+                    "${api.responseCodes.badRequest.description}"),
+            @ApiResponse(responseCode = "404", description =
+                    "${api.responseCodes.notFound.description}"),
+            @ApiResponse(responseCode = "422", description =
+                    "${api.responseCodes.unprocessableEntity.description}")
+    })
+    @GetMapping(value = "/", produces = "application/json")
     public Flux<Employee> getAllEmployees() {
         return employeeClient.getAllEmployees();
     }
 
-    @PostMapping
+    @Operation(
+            summary =
+                    "${api.employee-client.create-employee.description}",
+            description =
+                    "${api.employee-client.create-employee.notes}")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description =
+                    "${api.responseCodes.ok.description}"),
+            @ApiResponse(responseCode = "400", description =
+                    "${api.responseCodes.badRequest.description}"),
+            @ApiResponse(responseCode = "404", description =
+                    "${api.responseCodes.notFound.description}"),
+            @ApiResponse(responseCode = "422", description =
+                    "${api.responseCodes.unprocessableEntity.description}")
+    })
+    @PostMapping(value = "/", produces = "application/json")
     public Mono<Employee> createEmployee(@RequestBody Employee employee) {
         return employeeClient.createEmployee(employee);
     }
